73f68ecb6290215cbad109177ea08b846aca7022
idavvuk <ida.vranvuk@hotmail.com>
2025-02-04 16:13:08 +0100

    Added library component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,6 +4,9 @@ import { usePathname } from "next/navigation";
 import { useMemo } from "react";
 import { BiSearch } from "react-icons/bi";
 import { HiHome } from "react-icons/hi";
+import Box from "./Box";
+import SidebarItem from "./SidebarItem";
+import Library from "./Library";
 
 interface SidebarProps {
     children: React.ReactNode;
@@ -33,7 +36,7 @@ const Sidebar: React.FC<SidebarProps> = ({
         <div className="flex h-full">
             <div className="
             hidden
-            md-flex
+            md:flex
             flex-col
             gap-y-2
             bg-black
@@ -42,9 +45,28 @@ const Sidebar: React.FC<SidebarProps> = ({
             p-2
             ">
                 <Box>
-                    Sidebar Navigation
+                    <div
+                        className="
+                        flex
+                        flex-col
+                        gap-y-4
+                        px-5
+                        py-4">
+                            {routes.map((item) => (
+                                <SidebarItem 
+                                key={item.label}
+                                {...item}/>
+                            ))}
+
+                    </div>
+                </Box>
+                <Box className="overflow-y-auto h-full">
+                    <Library />
                 </Box>
             </div>
+            <main className="h-full flex-1 overflow-y-auto py-2">
+                {children}
+            </main>
         </div>
     );
 }
