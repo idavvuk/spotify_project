5d7aa3f70715298fbf4f17ad050d4077f728d585
idavvuk <ida.vranvuk@hotmail.com>
2025-02-04 17:07:02 +0100

    Added button layout for sign up and log in

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <Header>
         Header
       </Header>
-      
+
     </div>
   )
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,7 @@ import { BiSearch } from "react-icons/bi";
 import { HiHome } from "react-icons/hi";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { twMerge } from "tailwind-merge";
+import Button from "./Button";
 
 interface HeaderProps {
     children: React.ReactNode;
@@ -36,7 +37,7 @@ const Header: React.FC<HeaderProps> = ({
             mb-4
             flex
             items-center
-            justofy-between">
+            justify-between">
                 <div className="
             hidden
             md:flex
@@ -72,7 +73,7 @@ const Header: React.FC<HeaderProps> = ({
                 </div>
                 <div className="flex md:hidden gap-x-2 items-center">
                     <button
-                    className="
+                        className="
                     rounded-full
                     p-2
                     bg-white
@@ -84,7 +85,7 @@ const Header: React.FC<HeaderProps> = ({
                         <HiHome className="text-black" size={20} />
                     </button>
                     <button
-                    className="
+                        className="
                     rounded-full
                     p-2
                     bg-white
@@ -95,6 +96,33 @@ const Header: React.FC<HeaderProps> = ({
                     transition">
                         <BiSearch className="text-black" size={20} />
                     </button>
+                </div>
+                <div className="flex 
+                justify-between 
+                items-center 
+                gap-x-4">
+                    <>
+                        <div>
+                            <Button 
+                            onClick={() => {}}
+                            className="
+                        bg-transparent
+                        text-neutral-300
+                        font-medium">
+                                Sign Up
+                            </Button>
+                        </div>
+                        <div>
+                            <Button 
+                            onClick={() => {}}
+                            className="
+                        bg-white
+                        px-6
+                        py-2">
+                                Log In
+                            </Button>
+                        </div>
+                    </>
 
                 </div>
 
