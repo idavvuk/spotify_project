800935cb3ac2969e81b0fe1e0de06255b0138f6e
idavvuk <ida.vranvuk@hotmail.com>
2025-02-05 09:20:25 +0100

    added supabase database

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
             Welcome back
           </h1>
           <div
-          className="
+            className="
           grid
           grid-cols-1
           sm:grid-cols-2
@@ -26,16 +26,24 @@ export default function Home() {
           2xl:grid-cols-4
           gap-3
           mt-4">
-            <ListItem 
-            image="/images/liked.png"
-            name="Liked Songs"
-            href="Liked"/>
+            <ListItem
+              image="/images/liked.png"
+              name="Liked Songs"
+              href="Liked" />
 
           </div>
 
         </div>
       </Header>
-
+      <div className="mt-2 mb-7 px-6">
+        <div className="flex justofy-between items-center">
+          <h1 className="text-white text-2xl font-semibold">
+            Newest Songs
+          </h1>
+        </div>
+        <div>List of Songs!
+        </div>
+      </div>
     </div>
   )
 }
